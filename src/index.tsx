1965fe1694902858964ea75feb7ac77c0cc65c1b
Mayur B Patel <mayurpatel98765@gmail.com>
2023-02-02 12:19:38 +0530

    Add : ga4 added

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import ReactGA from "react-ga4";
 
+ReactGA.initialize("G-RNM9RMPSC8");
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
