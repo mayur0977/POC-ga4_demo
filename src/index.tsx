df8c234949ab1f99427abbce08eeb2b36407f54e
Mayur B Patel <mayurpatel98765@gmail.com>
2023-02-06 15:07:30 +0530

    Add : web vitals config for GA added

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,16 @@ import reportWebVitals from "./reportWebVitals";
 import ReactGA from "react-ga4";
 
 ReactGA.initialize("G-RNM9RMPSC8");
+
+function sendToAnalytics({ id, name, value }: any) {
+  ReactGA.send({
+    eventCategory: "Web Vitals",
+    eventAction: name,
+    eventValue: Math.round(name === "CLS" ? value * 1000 : value), // values must be integers
+    eventLabel: id, // id unique to current page load
+    nonInteraction: true, // avoids affecting bounce rate
+  });
+}
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
@@ -18,4 +28,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(sendToAnalytics);
