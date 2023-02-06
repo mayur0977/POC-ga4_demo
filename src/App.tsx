4fd4c6e282dccd7a1111661d20aa23a5ee499a9a
Mayur B Patel <mayurpatel98765@gmail.com>
2023-02-06 15:59:07 +0530

    Add : click button custom event added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import "./App.css";
-
+import ReactGA from "react-ga4";
 function App() {
   const click1Handle = () => {
     console.log("Click 1");
+    //click_button_one
+    ReactGA.event({
+      category: "User",
+      action: "click_button_one",
+    });
   };
 
   const click2Handle = () => {
